a905d5019841593046db2942781a1ee5dd4b71a7
MrSabit <miftahulislamsabit2@gmail.com>
2026-01-15 15:09:13 +0600

    Password Hash Increased

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(512))
     role = db.Column(db.String(10), default='student')
     profile_pic = db.Column(db.String(255), nullable=True, default='default.jpg')
     total_score = db.Column(db.Integer, default=0)
